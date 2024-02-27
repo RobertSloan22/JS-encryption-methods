2e2527788d0ca3aaaab827a8b3057b66a5469703
Robert Sloan <89170263+RobertSloan22@users.noreply.github.com>
2024-02-27 14:18:53 -0600

    Update mongodb.js

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://rstechsolutionsmeco:mitcapstone2023@cluster0.5qgxggh.mongodb.net/?retryWrites=true&w=majority";
+const uri = "  ";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
